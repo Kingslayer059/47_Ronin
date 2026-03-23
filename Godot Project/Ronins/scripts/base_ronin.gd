d809b1eab1bff081944768e52a5c290790394549
yutakastep <127261743+yutakastep@users.noreply.github.com>
2026-03-23 03:59:08 -0400

    VERY rudimentary title screen implementation
    also fixed thing where game would crash when loading with kunai ronin

--- Godot Project/Ronins/scripts/base_ronin.gd
+++ Godot Project/Ronins/scripts/base_ronin.gd
@@ -4,23 +4,23 @@ class_name BaseRonin extends CharacterBody2D
 #mostly for simplifying powerup implementation functions
 #can also be used for universal ronin functions
 
-@export var speed = 60
-@export var jump_speed = -200
-@export var gravity = 500
+@export var speed: float = 60
+@export var jump_speed: float = -200
+@export var gravity: float = 500
 @export var attack_speed: float = 1.0
 
 # function to increase speed for item pickups
 # might need a global item pickup manager for all the ronin
 # if we're being lazy honestly we could just copy paste item effects for all the ronin
 # unless items would have different effects for different ronin
-func increase_speed(spd_inc_amt):
+func increase_speed(spd_inc_amt) -> void:
 	speed += spd_inc_amt
 	print("increase speed by: ", spd_inc_amt)
 
-func increase_jumpHeight(jumpHeight_inc_amt):
+func increase_jumpHeight(jumpHeight_inc_amt) -> void:
 	gravity -= jumpHeight_inc_amt
 	print("decrease gravity by:", jumpHeight_inc_amt)
 
-func increase_attackSpeed(attackSpeed_inc_amt):
+func increase_attackSpeed(attackSpeed_inc_amt) -> void:
 	attack_speed *= attackSpeed_inc_amt
 	print("increase attack speed by: *", attackSpeed_inc_amt)
---
+++ Godot Project/TitleScreen/title_scene.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+const START_LEVEL: String = "res://Floor1/scenes/floor1.tscn"
+
+@onready var button_start = $CanvasLayer/Control/ButtonStart
+@onready var button_exit = $CanvasLayer/Control/ButtonExit
+
+func _ready():
+	
+	set_title_screen()
+	close_title_screen()
+	
+	pass
+
+func set_title_screen():
+	button_start.pressed.connect(start_game)
+	pass
+
+func start_game():
+	# TODO: Insert code here to load the first floor or homebase
+	
+	pass
+
+func close_title_screen():
+	button_exit.pressed.connect(close_game)
+
+func close_game():
+	get_tree().quit()
+	pass

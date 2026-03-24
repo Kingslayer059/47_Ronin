b50a9372879459b7adfc920e06af9391556ce639
yutakastep <127261743+yutakastep@users.noreply.github.com>
2026-03-24 14:46:23 -0400

    added item drops for enemies
    How to set item drops
    - go to inspector on enemy scene
    - increase size to how many items you want
    - select ItemDropData for each
    - Quick Load which item you want
    - set probability, min amt, max amt

---
+++ Godot Project/Floor1/Enemies/scripts/base_enemy_floor1.gd
@@ -0,0 +1,42 @@
+class_name Floor1Enemies extends CharacterBody2D
+
+# consolidates universal floor one enemy interactions
+# mainly created so that they drop specifically level 1 items
+
+const PICKUP = preload("res://Items/item_pickup/ItemPickup.tscn")
+@export_category("Item Drops")
+@export var drops: Array[ItemDropData]
+# IMPORTANT NOTE: MUST SET ITEM ARRAY FOR EACH ENEMY SCENE IN INSPECTOR
+
+@export_category("Health")
+@export var max_health: int = 3
+var health: int
+
+signal died(enemy)
+
+func _ready():
+	health = max_health
+
+func take_damage(amount: int) -> void:
+	health -= amount
+	if health <= 0:
+		death()
+
+func death() -> void:
+	died.emit(self)
+	drop_items()
+	queue_free()
+
+func drop_items() -> void:
+	if drops.is_empty():
+		return
+	for drop_data in drops:
+		if drop_data == null or drop_data.item == null:
+			continue
+		var drop_count: int = drop_data.get_drop_count()
+		for i in range(drop_count):
+			var drop: ItemPickup = PICKUP.instantiate()
+			drop.item_data = drop_data.item
+			drop.global_position = global_position
+			get_parent().call_deferred("add_child", drop)
+	pass
